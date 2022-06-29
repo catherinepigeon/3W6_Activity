d45bbb03ee2f3156bec029f36c7ac9c12fb016cb
catherinepigeon <1606840@cegepmontpetit.ca>
2022-06-29 13:30:08 -0400

    FCT : Création de la classe CalendarEvent

---
+++ S16D_Services/CrazyBooks_Models/Models/CalendarEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyBooks_Models.Models
+{
+    public class CalendarEvent
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string EventName {get;set;}
+    }
+}
